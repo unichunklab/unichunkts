34bc92a0af4760f7d8cb31c4dbf4432b401d591e
sdamadi <sdamadi1@umbc.edu>
2025-01-28 16:04:08 +0000

    Add documentation setup and gitignore

---
+++ docs/_sources/index.rst.txt
@@ -0,0 +1,17 @@
+.. Unichunk Time Series Forecasting documentation master file, created by
+   sphinx-quickstart on Tue Jan 28 15:53:53 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Unichunk Time Series Forecasting documentation
+==============================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+project = 'Unichunk Time Series Forecasting'
+copyright = '2025, Unichunk LLC'
+author = 'Unichunk LLC'
+
+# -- General configuration ---------------------------------------------------
+extensions = [
+    'sphinx.ext.autodoc',     # For automatically documenting Python modules
+    'sphinx.ext.napoleon',    # For parsing Google-style docstrings
+    'sphinx.ext.viewcode',    # For adding links to source code
+    'sphinx.ext.githubpages', # For GitHub Pages compatibility
+    'myst_parser',           # For Markdown support
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'  # Using Read the Docs theme instead of alabaster
+html_static_path = []
+html_show_sourcelink = True
+html_theme_options = {
+    'navigation_depth': 4,
+    'titles_only': False
+}
+
+# -- Napoleon settings -----------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
