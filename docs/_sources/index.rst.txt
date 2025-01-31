ecebf85d5609a63cfc8ca644d58d3b9a4125c868
sdamadi <sdamadi1@umbc.edu>
2025-01-31 04:29:21 +0000

    checking again 03

--- docs/_sources/index.rst.txt
+++ docs/_sources/index.rst.txt
@@ -1,5 +1,5 @@
 .. t documentation master file, created by
-   sphinx-quickstart on Fri Jan 31 04:15:55 2025.
+   sphinx-quickstart on Fri Jan 31 04:26:35 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +10,25 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 't'
-copyright = '2025, t'
-author = 't'
-release = 't'
+project = 'Unichunk Trading System'
+copyright = '2024, Unichunk'
+author = 'Unichunk'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',  # For viewing source code
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
